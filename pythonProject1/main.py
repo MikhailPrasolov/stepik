f4ad169ac20c0b17d897650109453c5ea1b345cb
MikhailPrasolov <fasesafe.1@gmail.com>
2023-01-22 12:04:58 +0300

    Редактирование file1.py

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -3,10 +3,12 @@ import json
 file = open('group_people.json', 'r')
 data = json.load(file)
 
-for id in data:
-    people = id['people']
-    k = 0
-    for person in people:
-        if person['gender'] == 'Female' and person['year'] >= 1977:
-            k += 1
-    print(id['id_group'], k)
+# for id in data:
+#     people = id['people']
+#     k = 0
+#     for person in people:
+#         if person['gender'] == 'Female' and person['year'] >= 1977:
+#             k += 1
+#     print(id['id_group'], k)
+
+print(data)
